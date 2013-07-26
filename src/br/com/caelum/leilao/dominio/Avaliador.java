9637b451a4df4be3a88a0fc8f01871d04c2e653e
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-07-26 11:29:04 -0300

    Alteração pré exercicio

--- src/br/com/caelum/leilao/dominio/Avaliador.java
+++ src/br/com/caelum/leilao/dominio/Avaliador.java
@@ -1,35 +1,46 @@
 package br.com.caelum.leilao.dominio;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Avaliador {
-
-	double maiorDeTodos = Double.NEGATIVE_INFINITY;
-	double menorDeTodos = Double.POSITIVE_INFINITY;
-	double media = 0;
-
-	public void avalia(Leilao leilao) {
-		double soma = 0;
-
-		for (Lance lance : leilao.getLances()) {
-			if (lance.getValor() > maiorDeTodos)
-				maiorDeTodos = lance.getValor();
-			if (lance.getValor() < menorDeTodos)
-				menorDeTodos = lance.getValor();
-			soma += lance.getValor();
-		}
-		this.media = soma / leilao.getLances().size();
-	}
-
-	public double getMaiorDeTodos() {
-		return maiorDeTodos;
-	}
-
-	public double getMenorDeTodos() {
-		return menorDeTodos;
-	}
-
-	public Double getMediaLances() {
-
-		return this.media;
-	}
-
+	  
+    private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+    private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private List<Lance> maiores;
+  
+    public void avalia(Leilao leilao) {
+        for(Lance lance : leilao.getLances()) {
+            if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
+            if (lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
+        }
+          
+        pegaOsMaioresNo(leilao);
+    }
+  
+    private void pegaOsMaioresNo(Leilao leilao) {
+        maiores = new ArrayList<Lance>(leilao.getLances());
+        Collections.sort(maiores, new Comparator<Lance>() {
+            public int compare(Lance o1, Lance o2) {
+                if(o1.getValor() < o2.getValor()) return 1;
+                if(o1.getValor() > o2.getValor()) return -1;
+                return 0;
+            }
+        });
+        maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
+    }
+      
+    public List<Lance> getTresMaiores() {
+        return this.maiores;
+    }
+      
+    public double getMaiorDeTodos() {
+        return maiorDeTodos;
+    }
+      
+    public double getMenorDeTodos() {
+        return menorDeTodos;
+    }
 }
